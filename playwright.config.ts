ba48fc355720ceb579c2669c29c50a3f7d02d206
BlurryFace1900 <debojitchowdhury98@gmail.com>
2024-10-25 21:37:45 +0530

    Third Commit

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,17 +19,21 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
+  timeout: 2 * 60 * 1000,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
+  
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'https://testpages.herokuapp.com/styled/tag/dynamic-table.html',
+    video: 'on',
     launchOptions: {
       slowMo: 1000,
     },
+    
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
   },
